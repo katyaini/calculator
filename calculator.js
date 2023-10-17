6b25de80f3e6304698073eca628b5d2fbd4670f2
Katyaini <katyaini.k@gmail.com>
2023-10-17 20:21:34 +0530

    with error messaged

--- calculator.js
+++ calculator.js
@@ -41,52 +41,81 @@ function operate(a,b,c)
 var newArray =[];
 var array =[];
 var string = "";
-
- var allNumberAndOperandButtons = document.querySelectorAll(".numbers");
+var allNumberAndOperandButtons = document.querySelectorAll(".numbers");
+var decimalButton = document.querySelector(".decimal");
  
  allNumberAndOperandButtons.forEach(element=>element.addEventListener("click", (e) =>{
    var value = e.target.innerHTML;
+  
 
-   if (value != "*" || value != "-" || value != "/" || value != "+" ) {
+   if (value != "*" || value != "-" || value != "/" || value != "+") {
      string = string + e.target.dataset.value;  
      newArray = array.slice();
      newArray.push(string);
+     
+     document.querySelector(".display").innerHTML = string;
+    
+
    }
    
    if(value === "*" || value === "-" || value === "/" || value === "+"){
     newArray.push(e.target.dataset.value)
+    document.querySelector(".display").innerHTML = e.target.dataset.value;
     
    }
 
+   if(e.target.dataset.value === ".")
+{
+    string = string + e.target.dataset.value;  
+     newArray = array.slice();
+     newArray.push(string);
+     console.log(newArray);
+}
 return newArray;
 
    
 }
  ));
- 
+
 var equals = document.querySelector(".equals");
 equals.addEventListener("click", ()=>{
+   
     var arrayToOperateOn= newArray.toString().split(/([-+*\/])/g);
     var arr = arrayToOperateOn.slice();
+    
     var arrayTobeSliced =[];
     var result = 0;
     var i = 0;
+    
 
 
     for (let i = 0; i <= arr.length ; i++){
         
-         if (arr[i] === "*" || arr[i] === "-" || arr[i] === "+" ||arr[i] === "/"){
+         if (arr[i] === "*" || arr[i] === "-" || arr[i] === "+"){
              result =  operate(parseInt(arr[i-1]), parseInt(arr[i+1]), arr[i]); 
              arrayTobeSliced = arr.splice(0,3,result);
              i--;
 
              document.querySelector(".display").innerHTML = result;
           }
-        }
-        console.log( result);
-    }
+          if (arr[i] === "/"){
+            if(arr[i+i] === "0"){
+                document.querySelector(".display").innerHTML= "you cant divide a number by 0, unless you want infinity . Hint:you dont!";
+
+            }
+            else{
+                result = operate(parseInt(arr[i-1], parseInt(arr[i+1]),arr[i]))
+                i--;
+                document.querySelector(".display").innerHTML = result;
+            }
+          }
+        
     
-    );
+    }
+});
+
+var decimalButton = document.querySelector(".decimal");
+
  
 var buttonClear = document.querySelector(".clearButton");
 
