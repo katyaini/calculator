f6c6cde4600b64e25b9e2297cae309492210271c
Katyaini <katyaini.k@gmail.com>
2023-10-24 02:36:30 +0530

    with modulus operator/function

--- calculator.js
+++ calculator.js
@@ -10,6 +10,10 @@ function multiply(a,b){
 function divide(a,b){
     return parseFloat((a / b).toFixed(2));
 }
+function modulus(a,b){
+    return parseFloat((a % b).toFixed(2));
+}
+
 
 var firstNumber = "";
 var secondNumber = "";
@@ -31,6 +35,9 @@ function operate(a,b,c)
     if(c === "/") {
          return divide(a,b);
     }
+    if(c === "%"){
+        return modulus(a,b);
+    }
 }
 
 var newArray =[];
@@ -42,14 +49,14 @@ var allNumberAndOperandButtons = document.querySelectorAll(".numbers");
  allNumberAndOperandButtons.forEach(element=>element.addEventListener("click", (e) =>{
    var value = e.target.innerHTML;
   
-   if (value != "*" || value != "-" || value != "/" || value != "+") {
+   if (value != "*" || value != "-" || value != "/" || value != "+" || value != "%") {
      string = string + e.target.dataset.value;  
      newArray = array.slice();
      newArray.push(string);
      document.querySelector(".display").innerHTML = string; 
    }
    
-   if(value === "*" || value === "-" || value === "/" || value === "+"){
+   if(value === "*" || value === "-" || value === "/" || value === "+" || value === "%"){
     newArray.push(e.target.dataset.value)
     document.querySelector(".display").innerHTML = e.target.dataset.value;
     console.log(newArray);
@@ -64,14 +71,14 @@ var equals = document.querySelector(".equals");
 equals.addEventListener("click", ()=>{
     var arrayTobeSliced = [];
      backSpace.disabled=true;
-    var arrayToOperateOn= newArray.toString().split(/([-+*\/])/g);
+    var arrayToOperateOn= newArray.toString().split(/([-+*%\/])/g);
     var arr = arrayToOperateOn.slice();
     var result = 0;
     var i = 0;
     
     for (let i = 0; i <= arr.length ; i++){
         
-         if (arr[i] === "*" || arr[i] === "-" || arr[i] === "+"){
+         if (arr[i] === "*" || arr[i] === "-" || arr[i] === "+" ){
              result =  operate(parseInt(arr[i-1]), parseInt(arr[i+1]), arr[i]); 
               arrayTobeSliced = arr.splice(0,3,result);
              i--;
@@ -89,7 +96,18 @@ equals.addEventListener("click", ()=>{
                 document.querySelector(".display").innerHTML = result;
                 string=result;
             }
-          }      
+          } 
+          if (arr[i] === "%"){
+            if(arr[i+i] === "0"){
+                document.querySelector(".display").innerHTML= "Oops!";
+            }
+            else{
+                result = operate(parseInt(arr[i-1], parseInt(arr[i+1]),arr[i]))
+                i--;
+                document.querySelector(".display").innerHTML = result;
+                string=result;
+            }
+          }     
     }
     
 });
